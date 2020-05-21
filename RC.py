85f41609f2f910c6fefc012ae2ff9abf9d41187b
Lucas Fernandes <lucasmatdlx@hotmail.com>
2020-05-21 15:40:51 -0300

    Update RC.py

--- RC.py
+++ RC.py
@@ -1,5 +1,5 @@
 class RC:
     youtube_link = "https://www.youtube.com/watch?v=zwvWrHdw7tU&feature=youtu.be"
-    chromedriver_path="Chrome/chromedriver.exe"
+    chromedriver_path="Chrome/chromedriver"
 
 
