b303b7ad712501556d176d4febe04e3be9e24070
Lucas Fernandes <lucasmatdlx@hotmail.com>
2020-05-21 00:53:39 -0300

    Update newproxy.py

--- newproxy.py
+++ newproxy.py
@@ -7,8 +7,9 @@ class youtube_bot():
     def __init__(self):
         options = Options()
         chrome_options = Options()
-        chrome_options.add_argument('--headless')
         chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--headless')
+        
         chrome_options.add_argument('--disable-dev-shm-usage')
 
         PROXY = "216.244.74.138:19006"
