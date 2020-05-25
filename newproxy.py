0738982c4b05db522a3c2c7e313576f53af31581
Lucas Fernandes <lucasmatdlx@hotmail.com>
2020-05-25 08:20:43 -0300

    Update newproxy.py

--- newproxy.py
+++ newproxy.py
@@ -6,14 +6,11 @@ from RC import RC
 class youtube_bot():
 
     def __init__(self):
-        options = Options()
-        options.add_experimental_option("detach", True)
-        options.add_argument("--headless")
+        self.options = Options()
+        self.options.add_experimental_option("detach", True)
 
-
-
-        PROXY = "216.244.74.138:19006"
-        options.add_argument('--proxy-server=%s' % PROXY)
+        PROXY = "163.172.221.119:19002"
+        self.options.add_argument('--proxy-server=%s' % PROXY)
         webdriver.DesiredCapabilities.CHROME['proxy'] = {
             "httpProxy": PROXY,
             "ftpProxy": PROXY,
@@ -22,11 +19,13 @@ class youtube_bot():
             "proxyType": "MANUAL",
             "autodetect": False
         }
-        self.driver = webdriver.Chrome(executable_path=RC.chromedriver_path,options=options)
+        self.driver = webdriver.Chrome(executable_path=RC.chromedriver_path, options=self.options)
         self.driver.get(RC.youtube_link)
 
+
     def open_youtube_video(self):
-        time.sleep(20)
+
+        time.sleep(30)
         #paste youtube inside of self.driver.get
         try:
             self.driver.find_element_by_xpath('/html/body/ytd-app/div/ytd-page-manager/ytd-watch-flexy/div[4]/div[1]/div/div[1]/div/div/div/ytd-player/div/div/div[4]/button').click()
@@ -34,19 +33,22 @@ class youtube_bot():
         except:
             pass
         time.sleep(2)
-        print("ok")
-
+        print("Video is playing")
+        self.driver.refresh()
 
     def close_youtube_video(self):
         self.driver.refresh()
 
 
 while True:
-    a = youtube_bot()
-    a.open_youtube_video()
-    time.sleep(310)
-    a.close_youtube_video()
-    time.sleep(310)
+
+        a = youtube_bot()
+        a.open_youtube_video()
+        time.sleep(30)
+        print("Ran 1 time")
+        a.close_youtube_video()
+        time.sleep(30)
+        print("It ran 1 more time successfully")
 
 
 
