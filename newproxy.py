b9523d2a4d78ac82f4359ecec2bc442014be2d0e
Lucas Fernandes <lucasmatdlx@hotmail.com>
2020-05-25 08:42:35 -0300

    Update newproxy.py

--- newproxy.py
+++ newproxy.py
@@ -11,6 +11,7 @@ class youtube_bot():
 
         PROXY = "163.172.221.119:19002"
         self.options.add_argument('--proxy-server=%s' % PROXY)
+        self.options.addArguments("--no-sandbox");
         self.options.add_argument('--disable-dev-shm-usage')
         webdriver.DesiredCapabilities.CHROME['proxy'] = {
             "httpProxy": PROXY,
